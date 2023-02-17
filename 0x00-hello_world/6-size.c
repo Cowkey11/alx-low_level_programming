7c56800c789f7b0bb78ed01f1967aceeb45d2533
Cowkey11 <wasongaokeyo3@gmail.com>
2023-02-16 18:25:40 -0800

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
  * mai - A program that prints the sizes of various computer types
  * Return: 0 (success)
  */
-int main(void) {
+int main(void) 
+	{
 	char a;
 	int b;
 	long int c;
@@ -16,5 +17,5 @@ int main(void) {
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-}
+	}
 
