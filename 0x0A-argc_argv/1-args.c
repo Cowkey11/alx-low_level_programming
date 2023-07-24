0ab90c9c4b00eee59dde554c36cb29c7a922069e
Cowkey11 <122993226+Cowkey11@users.noreply.github.com>
2023-07-24 04:01:31 -0700

    Args T.2 fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /*
- * program prints number of CML arguments passed
+ * program prints number of CML arguments passed to it
  *
  * return - 0 if successful, otherwise error.
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	int arguments_no;
+
+	arguments_no = argc - 1;
+	printf("%d\n", arguments_no);
 	return (0);
 }
