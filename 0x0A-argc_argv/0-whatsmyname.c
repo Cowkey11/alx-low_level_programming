1e7ebbf2c5bc9bea7750a65bb25e67eb59559151
Cowkey11 <122993226+Cowkey11@users.noreply.github.com>
2023-07-24 02:52:25 -0700

    Args T. 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+ * program prints out its name(first argument)
+ *
+ * return - 0 if successful, otherwise error.
+ */
+
+
+int main(int argc __attribute__((unused)), char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
