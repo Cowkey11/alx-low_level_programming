ac73b6ca871120e7e237c8c57e5069314c141387
Cowkey11 <122993226+Cowkey11@users.noreply.github.com>
2023-07-24 03:04:33 -0700

    Args T. 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * program prints number of CML arguments passed
+ *
+ * return - 0 if successful, otherwise error.
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
